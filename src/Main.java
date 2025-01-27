caff20bc73af36cfe0874a5671013ca93dc5a4ea
Elvin <seyfullazade.elvin@570gmail.com>
2025-01-28 01:04:00 +0400

    Revert "commit geri alındı"
    This reverts commit 795826af45aff06351464577ea16f25e658f9692.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("branch1 yaradildi ve chapa verildi");
-        System.out.println("burada mən intellij ilə pr atacam");
     }
 }
