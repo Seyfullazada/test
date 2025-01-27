0ba54fffadb6d5226dd7c27468f2e5fcf20edc82
Elvin <seyfullazade.elvin@570gmail.com>
2025-01-28 00:25:33 +0400

    yeni bir setir əlavə edildi.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("branch1 yaradildi ve chapa verildi");
+        System.out.println("burada mən intellij ilə pr atacam");
     }
 }
